e8b691ec8222cb81e5e4025bd9770de41cc48ac4
Ritika Agarwal <ritika26.agarwal@gmail.com>
2018-05-20 12:13:14 +0530

    Delete 07_nltk_split_sentences.py

--- 07_nltk_split_sentences.py
+++
@@ -1,9 +0,0 @@
-from nltk import sent_tokenize
-# load data
-filename = 'metamorphosis_clean.txt'
-file = open(filename, 'rt')
-text = file.read()
-file.close()
-# split into sentences
-sentences = sent_tokenize(text)
-print(sentences[0])
