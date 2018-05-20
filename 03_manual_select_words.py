ef98229f0ff80f193528d1076cdf3719137bcaa6
Ritika Agarwal <ritika26.agarwal@gmail.com>
2018-05-20 12:11:04 +0530

    Delete 03_manual_select_words.py

--- 03_manual_select_words.py
+++
@@ -1,9 +0,0 @@
-import re
-# load text
-filename = 'metamorphosis_clean.txt'
-file = open(filename, 'rt')
-text = file.read()
-file.close()
-# split based on words only
-words = re.split(r'\W+', text)
-print(words[:100])
