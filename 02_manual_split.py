20d25200599c3958ab76d89df5bfd3d944db3f08
Ritika Agarwal <ritika26.agarwal@gmail.com>
2018-05-20 12:10:40 +0530

    Delete 02_manual_split.py

--- 02_manual_split.py
+++
@@ -1,8 +0,0 @@
-# load text
-filename = 'metamorphosis_clean.txt'
-file = open(filename, 'rt')
-text = file.read()
-file.close()
-# split into words by white space
-words = text.split()
-print(words[:100])
