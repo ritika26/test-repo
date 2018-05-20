ce50271f0e3d8c5e1866f588cc197f112333d6d1
Ritika Agarwal <ritika26.agarwal@gmail.com>
2018-05-20 12:09:59 +0530

    Delete 01_load_file.py

--- 01_load_file.py
+++
@@ -1,8 +0,0 @@
-# load one file
-filename = 'txt_sentoken/neg/cv000_29416.txt'
-# open the file as read only
-file = open(filename, 'r')
-# read all text
-text = file.read()
-# close the file
-file.close()
