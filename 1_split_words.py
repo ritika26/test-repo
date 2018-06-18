a715da8789aa6e9565296beb941dfe34b42e53c7
Ritika Agarwal <ritika26.agarwal@gmail.com>
2018-06-18 08:49:21 +0530

    Delete 1_split_words.py

--- 1_split_words.py
+++
@@ -1,6 +0,0 @@
-from keras.preprocessing.text import text_to_word_sequence
-# define the document
-text = 'The quick brown fox jumped over the lazy dog.'
-# tokenize the document
-result = text_to_word_sequence(text)
-print(result)
