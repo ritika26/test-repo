467c98916870ecb788097cb4445c5c4a37b78047
Ritika Agarwal <ritika26.agarwal@gmail.com>
2018-06-25 08:54:50 +0530

    Delete 2_prepare_vocab.py

--- 2_prepare_vocab.py
+++
@@ -1,7 +0,0 @@
-from keras.preprocessing.text import text_to_word_sequence
-# define the document
-text = 'The quick brown fox jumped over the lazy dog.'
-# estimate the size of the vocabulary
-words = set(text_to_word_sequence(text))
-vocab_size = len(words)
-print(vocab_size)
